43bf1d2f1a10ec46337023ff4419be9f4c2fedc6
Joe Polny <joepolny@gmail.com>
2021-04-04 21:32:20 -0400

    startLoops()

--- src/client/core.ts
+++ src/client/core.ts
@@ -9,20 +9,21 @@ export class Core {
     this.vehicles = new GTAP.Vehicles
     this.menus = new GTAP.Menus(this)
     this.registerEvents()
-
+    this.startLoops()
     emitNet('GTAPlusServer:updateMoney', 100)
-
-
-    Loop(0, () => {         
-      if (IsControlJustReleased(0, 27)) {
-        this.menus.menus['main'].open()
-      } 
-    })
   }
   
   registerEvents() {
     onNet('GTAPlusClient:updateMoney', (ammount: number) => {
       StatSetInt('BANK_BALANCE', ammount, true)
+    })
+  }
+
+  startLoops() {
+    Loop(0, () => {         
+      if (IsControlJustReleased(0, 27)) {
+        this.menus.menus['main'].open()
+      } 
     })
   }
   
